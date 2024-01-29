891148f58645c6930483cd073b981a6744b4f126
Skoteinos1 <p.nagy.11@gmail.com>
2024-01-29 19:36:39 +0000

    update

---
+++ Pikomat/ml_lesson.py
@@ -0,0 +1,12 @@
+# pip install scikit-learn
+# pip install scipy
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+# %matplotlib inline
+
+# our_data =  pd.
+
+plt.scatter
