cc3462a46d597a66fb49875300c0501ce8684f86
Skoteinos1 <p.nagy.11@gmail.com>
2024-02-09 22:09:41 +0000

    update

--- Filter_files_by_content.py
+++ Filter_files_by_content.py
@@ -1,13 +1,15 @@
+# Looks for files which contain all of listed strings
+# It looks only in .txt .xml and .json files
 import os
 
-path = '/home/skoty/Stable Diffusion UI/1701102498415/'
+path = '/PATH/TO/FOLDER//'
 
 strings_to_have = [
-    '1895996768',
-    'Generate an image of a 21-year-old blond woman with dark blue eyes. Capture her in a moment of quiet contentment, featuring a subtle and genuine suggestive smile. Emphasize a sense of calm and charm in her expression. In terms of makeup, she has strong pink blush shades under her eyes and dark bottom eyeliner, noticeable black lower lash lines. The setting should be neutral, and the lighting should be soft and natural to enhance the warmth of the scene. Please ensure the image is tasteful and avoids any explicit or inappropriate elements, focusing on highlighting her personality and charm.',
-    'BadDream, (UnrealisticDream:1.3), glasses, cap, hat, hoodie',
-    "absolutereality_v181",
-    '"use_lora_model": null,',
+    'FILE',
+    'MUST',
+    'CONTAIN',
+    "THESE",
+    'STRINGS',
 ]
 
 for path, subdirs, files in os.walk(path):
