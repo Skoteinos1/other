c025c7b46a98b47c08af46387062eff819655808
Skoteinos1 <155898084+Skoteinos1@users.noreply.github.com>
2024-06-08 21:43:35 +0100

    print colored text

--- _CheatSheet.py
+++ _CheatSheet.py
@@ -482,6 +482,20 @@ def load_pickle(fl_nm):
     return data1
 
 
+# Print in color ---------------------------------------------------------------------------------
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+print(bcolors.WARNING +'Some Text'+ bcolors.ENDC)
+
+
 # Regular Expresions - RegEx --------------------------------------------------------------------
 # Verify string patterns and perform substitutions in string
 import re
